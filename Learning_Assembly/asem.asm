16614ed151b71c8306733440964e52a30fd1c93b
tristanlok <tristanlok@outlook.com>
2024-03-28 01:25:27 -0400

    Learning how to use the Stack

--- Learning_Assembly/asem.asm
+++ Learning_Assembly/asem.asm
@@ -10,6 +10,8 @@ section .text
     global _start
 
 _start:
+    
+    ; Inputing and Outputting Text
     mov rsi, text1
     mov rdx, 19
     call _printText
@@ -23,27 +25,61 @@ _start:
     mov rdx, 16
     call _printText
 
+    ; Outputting Numbers and performing Arithmetic
     mov rax, 7
-    call _printDigit
+    call _printDigit ; 7
 
     mov rdx, 0
     mov rax, 6
     mov rbx, 3
     div rbx
 
-    call _printDigit
+    call _printDigit ; 2
 
     mov rax, 2
     mov rbx, 3
     mul rbx
 
-    call _printDigit
-
+    call _printDigit ; 6
 
     mov rax, 6
     add rax, 2
-    call _printDigit
+    call _printDigit ; 8
 
+    ; Stack Operations and storing values in the stack
+
+    ; Stack pointers can Increment/Decrement + Pre-/Post-
+    ; x86-64 decrements when we call push
+    mov rbp, rsp ; Change the base pointer (keeps track of the base of the current stack frame) to the top of the stack pointer
+
+    push 5
+
+    push 0xa ; ascii for newline
+
+    mov rax, "tristan"
+    push rax
+
+    push 1
+
+    pop rax
+    call _printDigit ; 1
+
+    mov rsi, rsp ; same as lea rsi, [rsp]
+    mov rdx, 7
+    call _printText ; tristan
+
+    add rsp, 8
+
+    mov rsi, rsp
+    mov rdx, 1
+    call _printText ; newline
+
+    add rsp, 2
+    
+    pop rax
+    call _printDigit ; 5
+
+    ; Ends the program
     call _endProgram
 
 _getName:
