e59f701df05966a53e01fe1b2a6ebb8318aba1b4
tristanlok <tristanlok@Outlook.com>
2024-04-04 21:27:19 -0400

    Cleaned up code

--- src/parser.c
+++ src/parser.c
@@ -49,62 +49,6 @@ static struct ASTnode *getPrimaryNode() {
     }
 }
 
-/*
-
-Parser w/ preceedence using Pratt's parser (Understanding)
-
-1 + 4 * 7 + 3
-
-Takes in the first int token (1)
-
-Scan the next token - move on if not EOF
-
-check if the current precedence is greater than the previous (+) 10 > 0
-
-scans the next token (4)
-
-Recursively calls itself giving the precidence of the operator (10)
-
-Takes in the first token (4)
-
-scan the next token - move on if not EOF
-
-check if the current precedence is greater that the previous (x) 20 > 10
-
-scans the next token (7)
-
-Recursively calls itself giving hte precidence of the operator (20)
-
-Takes in the first token (7)
-
-scan in the next token - move on if not EOF
-
-check if the current precedence is greater than the previous (+) 10 > 20 (false)
-
-returns back 7 as right
-
-creates the first subtree (multiplication 4*7)
-
-checks if the TOKEN (currently +) 10 > 20 (false)
-
-returns multiplcation subtree back to first function as right
-
-creates the second subtree [1 + (4 * 7)]
-
-checks if the TOKEN (currently +) 10 > 0
-
-scans the next token (3)
-
-since EOF after right will be 3
-
-creates the third subtree [1 + (4 * 7)] + 3
-
-since EOF
-
-return third subtree
-
-*/
-
 struct ASTnode *makeTree(int prevTokPrec) {
     struct ASTnode *leftn, *rightn;
     int nodeValue;
